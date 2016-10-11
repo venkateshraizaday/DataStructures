585a3f91a030b252d34331f0ee6a43265e27589a
Venkatesh Raizaday <venkatesh.raizaday@gmail.com>
2016-10-10 20:22:12 -0400

    Tree Implementation
    Tree Implementation

---
+++ Tree.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self,data,left=None,right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+class BinaryTree:
+    def __init__(self,root):
+        self.root = root
+
+    def insert(self):
