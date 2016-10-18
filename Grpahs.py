d799cf06348ccd634d464c42cef413f497622793
Venkatesh Raizaday <venkatesh.raizaday@gmail.com>
2016-10-18 11:13:13 -0400

    Added bfs and dfs implementation

--- Grpahs.py
+++ Grpahs.py
@@ -11,16 +11,46 @@ class Graph:
         self.nodes = nodes
 
     def printAll(self):
-        for node in self.nodes.values():
-            node.printDetails()
+        for node in self.nodes:
+            self.nodes[node].printDetails()
 
-n = 5
-edges = [(1,2),(2,3),(2,5),(4,5)]
+    def bfs(self,root):
+        print("Bredth First Search:-")
+        q = [root]
+        visited = {}
+        while len(q) != 0:
+            node = q.pop(0)
+            if node not in visited:
+                print(node, end=' ')
+                c = self.nodes[node].children
+                visited[node] = True
+                for child in c:
+                    q.append(child)
+        print()
+
+    def dfs(self,root):
+        print("Depth First Search:-")
+        q = [root]
+        visited = {}
+        while len(q) != 0:
+            node = q.pop()
+            if node not in visited:
+                print(node, end=' ')
+                c = self.nodes[node].children
+                visited[node] = True
+                for child in c:
+                    q.append(child)
+        print()
+
+n = 6
+edges = [(0,5),(0,4),(0,1),(1,4),(1,3),(2,1),(3,2),(3,4)]
 
 g = Graph()
 for i in range(n):
-    g.nodes[i+1] = Node(i+1,[])
+    g.nodes[i] = Node(i,[])
 for (u,v) in edges:
     g.nodes[u].children.append(v)
-    g.nodes[v].children.append(u)
+    #g.nodes[v].children.append(u)
 g.printAll()
+g.bfs(0)
+g.dfs(0)
